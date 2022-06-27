4fdf7066922f3be72ad9fabd3363f41fa151861d
luanaFOliveira <94805217+luanaFOliveira@users.noreply.github.com>
2022-06-27 15:34:26 -0300

    Add files via upload

---
+++ Roupa.java
@@ -0,0 +1,15 @@
+class Roupa{
+    protected String cor;
+    protected String tecido;
+    public Tecido(String cor,String tecido){
+        this.cor=cor;
+        this.tecido=tecido;
+    }
+    public String getCor(){
+        return cor;
+    }
+    public String getTecido(){
+        return tecido;
+    }
+}
+//da para criar tipo camiseta, calca,vestido,casaco,etc que podem ter tipo tamanho,estampa,etc
