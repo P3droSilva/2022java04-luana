6aedbe560e77cd930177fd9595981bae1ec777f0
Pedro Henrique da Silva Hinerasky <104012810+P3droSilva@users.noreply.github.com>
2022-06-27 16:06:52 -0300

    Create TestCamiseta.java

---
+++ TestCamiseta.java
@@ -0,0 +1,6 @@
+public class TestCamiseta{
+    public static void main(String[] args){
+        Camiseta c = new Camiseta("preto","algodao","AMONGUS","Adidas");
+        c.PrintCamiseta();
+    }
+}
